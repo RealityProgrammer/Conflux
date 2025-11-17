c7df7a3153b7766ce64c1e51d998b14bb0c32800
Caxivitual <destinyhero789@gmail.com>
2025-11-17 19:28:33 +0700

    refactor Dialog

---
+++ Conflux/Client/scripts/components/modal.ts
@@ -0,0 +1,38 @@
+﻿import { animate } from 'animejs';
+
+export function animateDialog(element: HTMLElement, mode: 'start' | 'end', delay: number, duration: number, ease: string) {
+    if (mode == 'start') {
+        element.hidden = false;
+    } else if (mode == 'end') {
+        element.hidden = true;
+    }
+    
+    // const animateProperties = {
+    //     start: {
+    //         top: '50%',
+    //         y: '-50%',
+    //     },
+    //     end: {
+    //         top: '100%',
+    //         y: '0%',
+    //     },
+    // };
+    //
+    // animate(element, {
+    //     ...animateProperties[mode],
+    //     delay: delay,
+    //     duration: duration,
+    //     ease: ease,
+    //     autoplay: true,
+    //     onBegin: function() {
+    //         if (mode === 'start') {
+    //             element.hidden = false;
+    //         }
+    //     },
+    //     onComplete: function() {
+    //         if (mode === 'end') {
+    //             element.hidden = true;
+    //         }
+    //     },
+    // });
+}
