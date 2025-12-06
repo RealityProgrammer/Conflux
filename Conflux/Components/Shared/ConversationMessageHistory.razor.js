350de00c20427e2bf9feb7c3688d8874f520a34f
Caxivitual <destinyhero789@gmail.com>
2025-12-06 17:53:48 +0700

    implements basic scroll load events

---
+++ Conflux/Components/Shared/ConversationMessageHistory.razor.js
@@ -0,0 +1,28 @@
+﻿export function initializeScrollContainer(element, dotNetHelper) {
+    let debounceTimer;
+    const threshold = 100;
+    const debounceDelay = 150;
+    
+    element.addEventListener("scroll", async () => {
+        clearTimeout(debounceTimer);
+        
+        debounceTimer = setTimeout(async () => {
+            const distanceFromBottom = element.scrollHeight - element.scrollTop - element.clientHeight;
+            const distanceFromTop = element.scrollTop;
+
+            if (distanceFromTop <= threshold) {
+                await dotNetHelper.invokeMethodAsync('HandleLoadTopMessages');
+            } else if (distanceFromBottom <= threshold) {
+                await dotNetHelper.invokeMethodAsync('HandleLoadBottomMessages');
+            }
+        }, debounceDelay);
+    })
+}
+
+export function getScrollTop(element) {
+    return element.scrollTop;
+}
+
+export function scrollToBottom(element) {
+    element.scrollTop = element.scrollHeight;
+}
