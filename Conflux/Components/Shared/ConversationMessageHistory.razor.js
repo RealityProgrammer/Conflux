5cabe19b383cffecb8714edb0965b3c9dea6980c
Caxivitual <destinyhero789@gmail.com>
2025-12-07 16:06:10 +0700

    fix visual bugs

--- Conflux/Components/Shared/ConversationMessageHistory.razor.js
+++ Conflux/Components/Shared/ConversationMessageHistory.razor.js
@@ -1,46 +1,72 @@
-﻿export function initializeScrollContainer(element, dotNetHelper) {
+﻿export function initializeScrollContainer(scrollContainer, dotNetHelper) {
     let debounceTimer;
     const threshold = 150;
     const debounceDelay = 150;
     
-    element.addEventListener("scroll", async () => {
+    scrollContainer.addEventListener("scroll", async () => {
         clearTimeout(debounceTimer);
         
         debounceTimer = setTimeout(async () => {
-            const distanceFromTop = element.scrollTop;
-            const distanceFromBottom = element.scrollHeight - element.scrollTop - element.clientHeight;
-
-            if (distanceFromTop <= threshold) {
+            if (scrollContainer.scrollTop <= threshold) {
                 await dotNetHelper.invokeMethodAsync('HandleLoadTopMessages');
-            } else if (distanceFromBottom <= threshold) {
+                console.log("HandleLoadTopMessages");
+            } else if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight - threshold) {
                 await dotNetHelper.invokeMethodAsync('HandleLoadBottomMessages');
             }
         }, debounceDelay);
     })
 }
 
-export function scrollToBottom(element) {
-    element.scrollTop = element.scrollHeight;
+export function scrollToBottom(scrollContainer) {
+    scrollContainer.scrollTop = scrollContainer.scrollHeight;
+    console.log("scrollToBottom: " + scrollContainer.scrollHeight);
 }
 
-export function saveScrollPosition(element) {
-    console.log("saveScrollPosition");
-    
-    const scrollTop = element.scrollTop;
-    const scrollHeight = element.scrollHeight;
+export function saveScrollPosition(scrollContainer) {
+    const scrollTop = scrollContainer.scrollTop;
+    const scrollHeight = scrollContainer.scrollHeight;
 
     return { scrollTop, scrollHeight };
 }
 
-export function restoreScrollPosition(element, savedScrollPosition) {
+export function restoreScrollPositionForTop(scrollContainer, savedScrollPosition) {
     requestAnimationFrame(() => {
-        console.log("restoreScrollPosition");
-        
         if (savedScrollPosition) {
-            const newScrollHeight = element.scrollHeight;
+            const newScrollHeight = scrollContainer.scrollHeight;
             const heightIncrease = newScrollHeight - savedScrollPosition.scrollHeight;
 
-            element.scrollTop = savedScrollPosition.scrollTop + heightIncrease;
+            scrollContainer.scrollTop = savedScrollPosition.scrollTop + heightIncrease;
         }
+    });
+}
+
+export function restoreScrollPositionForBottom(scrollContainer, savedScrollPosition) {
+    // No need to do math, because it is automatically taken care of somehow lmao.
+    
+    // requestAnimationFrame(() => {
+    //     if (savedScrollPosition) {
+    //         // const newScrollHeight = scrollContainer.scrollHeight;
+    //         // const heightIncrease = newScrollHeight - savedScrollPosition.scrollHeight;
+    //
+    //         // scrollContainer.scrollTop = savedScrollPosition.scrollTop + heightIncrease;
+    //        
+    //         // const newScrollHeight = scrollContainer.scrollHeight;
+    //         // const targetDistanceFromBottom = savedScrollPosition.distanceFromBottom;
+    //         //
+    //         // scrollContainer.scrollTop = newScrollHeight - savedScrollPosition.clientHeight - targetDistanceFromBottom;
+    //     }
+    // });
+}
+
+export function jumpToMessage(scrollContainer, id) {
+    const messageElement = scrollContainer.querySelector(`[data-message-id='${id}']`);
+    
+    if (!messageElement) return;
+    
+    messageElement.scrollIntoView({
+        behavior: 'instant',
+        block: 'center',
+        container: scrollContainer,
+        inline: 'nearest',
     });
 }
