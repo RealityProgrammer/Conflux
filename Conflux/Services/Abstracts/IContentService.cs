d33059e33bb32bceafcfee45462762bc1d017ac9
Caxivitual <destinyhero789@gmail.com>
2025-12-11 16:25:22 +0700

    added message attachment functions for IContentService

--- Conflux/Services/Abstracts/IContentService.cs
+++ Conflux/Services/Abstracts/IContentService.cs
@@ -1,7 +1,10 @@
 ﻿namespace Conflux.Services.Abstracts;
 
 public interface IContentService {
-    Task<string> UploadAvatarAsync(Stream stream, string userId);
+    Task<string> UploadAvatarAsync(Stream stream, string userId, CancellationToken cancellationToken = default);
     Task DeleteAvatarAsync(string userId);
     DateTime GetAvatarUploadTime(string userId);
+
+    Task<ICollection<string>> UploadMessageAttachmentsAsync(ICollection<Stream> attachmentStreams, CancellationToken cancellationToken = default);
+    Task<bool> DeleteMessageAttachmentAsync(string attachmentRelativePath);
 }
--- Conflux/Services/ContentService.cs
+++ Conflux/Services/ContentService.cs
@@ -3,17 +3,19 @@
 namespace Conflux.Services;
 
 public class ContentService(IWebHostEnvironment environment) : IContentService {
-    public async Task<string> UploadAvatarAsync(Stream stream, string userId) {
+    public async Task<string> UploadAvatarAsync(Stream stream, string userId, CancellationToken cancellationToken = default) {
+        cancellationToken.ThrowIfCancellationRequested();
+        
         string path = Path.Combine("avatar", userId);
         string physicalPath = Path.Combine(environment.ContentRootPath, "Uploads", path);
-        await using var destinationStream = File.OpenWrite(physicalPath);
         
+        await using var destinationStream = File.OpenWrite(physicalPath);
         destinationStream.SetLength(0);
         
-        await destinationStream.FlushAsync();
-        await stream.CopyToAsync(destinationStream);
+        await destinationStream.FlushAsync(cancellationToken);
+        await stream.CopyToAsync(destinationStream, cancellationToken);
 
-        return Path.Join("uploads", path);
+        return path;
     }
 
     public Task DeleteAvatarAsync(string userId) {
@@ -30,5 +32,50 @@ public class ContentService(IWebHostEnvironment environment) : IContentService {
         string physicalPath = Path.Combine(environment.ContentRootPath, "Uploads", path);
         
         return File.GetLastWriteTimeUtc(physicalPath);
+    }
+
+    public async Task<ICollection<string>> UploadMessageAttachmentsAsync(ICollection<Stream> attachmentStreams, CancellationToken cancellationToken = default) {
+        cancellationToken.ThrowIfCancellationRequested();
+        
+        List<string> outputPaths = new List<string>(attachmentStreams.Count);
+
+        try {
+            foreach (Stream stream in attachmentStreams) {
+                cancellationToken.ThrowIfCancellationRequested();
+                
+                string path = Path.Combine("msg_attachments", Guid.CreateVersion7().ToString());
+                string physicalPath = Path.Combine(environment.ContentRootPath, "Uploads", path);
+
+                await using var destinationStream = File.OpenWrite(physicalPath);
+                destinationStream.SetLength(0);
+
+                await destinationStream.FlushAsync(cancellationToken);
+                await stream.CopyToAsync(destinationStream, cancellationToken);
+
+                outputPaths.Add(path);
+            }
+
+            return outputPaths;
+        } catch {
+            foreach (string path in outputPaths) {
+                string physicalPath = Path.Combine(environment.ContentRootPath, "Uploads", path);
+                
+                File.Delete(physicalPath);
+            }
+
+            throw;
+        }
+    }
+
+    public Task<bool> DeleteMessageAttachmentAsync(string attachmentRelativePath) {
+        string physicalPath = Path.Combine(environment.ContentRootPath, "Uploads", attachmentRelativePath);
+
+        if (!attachmentRelativePath.StartsWith(Path.Combine(environment.ContentRootPath, "Uploads", "msg_attachments"))) {
+            return Task.FromResult(false);
+        }
+        
+        File.Delete(physicalPath);
+
+        return Task.FromResult(true);
     }
 }
