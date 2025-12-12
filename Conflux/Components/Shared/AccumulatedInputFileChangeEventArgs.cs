cd13351a6ebd0ee065d090abbab9b13fcbd8c4b3
Caxivitual <destinyhero789@gmail.com>
2025-12-12 21:54:31 +0700

    implemented a hacky way to bypass restriction of input file accumulation select

---
+++ Conflux/Components/Shared/AccumulatedInputFileChangeEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Conflux.Components.Shared;
+
+public sealed class AccumulatedInputFileChangeEventArgs : EventArgs {
+    public readonly InputFileChangeEventArgs ChangeEventArgs;
+    public readonly InputFile Owner;
+
+    public AccumulatedInputFileChangeEventArgs(InputFileChangeEventArgs changeEventArgs, InputFile owner) {
+        ChangeEventArgs = changeEventArgs;
+        Owner = owner;
+    }
+}
