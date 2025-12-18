575e97cd501ea2b6ee70164367151fbe50646fd2
Caxivitual <destinyhero789@gmail.com>
2025-12-18 15:44:59 +0700

    reimplementing a bunch of tooltip/popover UI related

--- Conflux/Client/scripts/utils.ts
+++ Conflux/Client/scripts/utils.ts
@@ -1,4 +1,6 @@
-﻿function createInputPreviewUrl(inputElement: HTMLInputElement, fileIndex: number) : string|null {
+﻿import { computePosition, flip, shift, arrow, offset, ComputePositionReturn, Placement } from "@floating-ui/dom";
+
+function createInputPreviewUrl(inputElement: HTMLInputElement, fileIndex: number) : string|null {
     if (!inputElement || !inputElement.files || inputElement.files.length < fileIndex) return null;
 
     if (!inputElement.files) {
@@ -11,10 +13,49 @@
     return URL.createObjectURL(file);
 }
 
+function updateTooltipPosition(targetElement: Element, tooltipElement: HTMLElement, arrowElement: HTMLElement | null, tooltipPlacement: Placement, tooltipOffset: number) {
+    computePosition(targetElement, tooltipElement, {
+        placement: tooltipPlacement,
+        middleware: [
+            flip({}),
+            shift(),
+            offset(tooltipOffset),
+
+            ...(arrowElement ? [ arrow({element: arrowElement, padding: 0 }) ] : []),
+        ],
+    }).then((position: ComputePositionReturn) => {
+        tooltipElement.style.left = `${position.x}px`;
+        tooltipElement.style.top = `${position.y}px`;
+        tooltipElement.removeAttribute("hidden");
+
+        const staticSide = {
+            top: 'bottom',
+            right: 'left',
+            bottom: 'top',
+            left: 'right',
+            // @ts-ignore
+        }[position.placement.split('-')[0]];
+
+        if (position.middlewareData.arrow && arrowElement) {
+            const {x, y} = position.middlewareData.arrow!;
+
+            Object.assign(arrowElement.style, {
+                left: x != null ? `${x}px` : '',
+                top: y != null ? `${y}px` : '',
+                right: '',
+                bottom: '',
+                [staticSide]: '-4px',
+            });
+        }
+    });
+}
+
 declare global {
     interface Window {
         createInputPreviewUrl: (inputElement: HTMLInputElement, fileIndex: number) => string|null;
+        updateTooltipPosition: (targetElement: Element, tooltipElement: HTMLElement, arrowElement: HTMLElement | null, tooltipPlacement: Placement, tooltipOffset: number) => void;
     }
 }
 
 window.createInputPreviewUrl = createInputPreviewUrl;
+window.updateTooltipPosition = updateTooltipPosition;
