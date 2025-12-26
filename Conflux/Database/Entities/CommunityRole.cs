dd296b2459719765e600a7bb0618e17fa8cc2b33
Caxivitual <destinyhero789@gmail.com>
2025-12-26 14:52:52 +0700

    add channel permission flags to CommunityRole

--- Conflux/Database/Entities/CommunityRole.cs
+++ Conflux/Database/Entities/CommunityRole.cs
@@ -10,7 +10,35 @@ public class CommunityRole : ICreatedAtColumn {
     public Guid CommunityId { get; set; }
     public Community Community { get; set; } = null!;
     
+    public RolePermissionFlags RolePermissions { get; set; }
+    public ChannelPermissionFlags ChannelPermissions { get; set; }
+    
     public DateTime CreatedAt { get; set; }
 
     public ICollection<CommunityMember> MembersWithRole { get; set; } = null!;
+
+    [Flags]
+    public enum RolePermissionFlags : byte {
+        None = 0,
+        
+        CreateRole = 1 << 0,
+        DeleteRole = 1 << 1,
+        ModifyRolePermissions = 1 << 2,
+        ModifyMemberRole = 1 << 3,
+        
+        All = CreateRole | DeleteRole | ModifyRolePermissions | ModifyMemberRole,
+    }
+    
+    [Flags]
+    public enum ChannelPermissionFlags : byte {
+        None = 0,
+    
+        CreateChannelCategory = 1 << 0,
+        DeleteChannelCategory = 1 << 1,
+    
+        CreateChannel = 1 << 2,
+        DeleteChannel = 1 << 3,
+        
+        All = CreateChannelCategory | DeleteChannelCategory | CreateChannel | DeleteChannel,
+    }
 }
---
+++ Conflux/Database/Migrations/20251225032630_AddChannelPermissionFlagsToCommunityRole.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conflux.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChannelPermissionFlagsToCommunityRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ChannelPermissionFlags",
+                table: "CommunityRoles",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChannelPermissionFlags",
+                table: "CommunityRoles");
+        }
+    }
+}
