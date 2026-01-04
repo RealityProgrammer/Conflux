875276dcc67c0b3f6736730052db690e9adc006c
Caxivitual <destinyhero789@gmail.com>
2026-01-04 21:00:08 +0700

    improve Sidebar component: Responsive, overlay, improve logic

--- Conflux/Client/scripts/Components/Sidebar.ts
+++ Conflux/Client/scripts/Components/Sidebar.ts
@@ -2,15 +2,29 @@
 
 const MAX_OVERLAY_OPACITY = 60;
 
-export function initializeSidebar(sidebarElement: HTMLElement, overlayElement: HTMLElement, interactingElement: HTMLElement | null): HammerManager {
+interface DisposeState {
+    hammer: HammerManager;
+    resizeUnregister: () => void;
+}
+
+export function initializeSidebar(sidebarElement: HTMLElement, overlayElement: HTMLElement): DisposeState {
     let isOpen = false, dragging = false, startX = 0, translate = -100;
 
+    const checkSurpassBreakpoint = () => {
+        return window.innerWidth >= 1024; // lg breakpoint
+    };
+    
+    let surpassedBreakpoint = checkSurpassBreakpoint();
+    
     const getWidth = () => sidebarElement.getBoundingClientRect().width;
 
     const open = () => {
+        if (surpassedBreakpoint || isOpen) return;
+        
         isOpen = true;
         translate = 0;
-        sidebarElement.style.transform = 'translateX(0)';
+        sidebarElement.classList.add('translate-x-0');
+        sidebarElement.classList.remove('-translate-x-full');
         
         overlayElement.classList.remove('invisible');
         overlayElement.style.opacity = `${MAX_OVERLAY_OPACITY}%`;
@@ -19,7 +33,8 @@ export function initializeSidebar(sidebarElement: HTMLElement, overlayElement: H
     const close = () => {
         isOpen = false;
         translate = -100;
-        sidebarElement.style.transform = 'translateX(-100%)';
+        sidebarElement.classList.remove('translate-x-0');
+        sidebarElement.classList.add('-translate-x-full');
 
         overlayElement.classList.add('invisible');
         overlayElement.style.opacity = '0%';
@@ -27,21 +42,24 @@ export function initializeSidebar(sidebarElement: HTMLElement, overlayElement: H
 
     overlayElement.onclick = close;
 
-    const hammer = new Hammer(interactingElement || document.body);
+    const hammer = new Hammer(document.body);
     hammer.get('pan').set({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 5 });
     
     hammer.on('panstart', (e: HammerInput) => {
+        if (surpassedBreakpoint) return;
+        
         if (e.center.x > 40 && !isOpen) return;
         dragging = true;
         startX = e.center.x;
 
-        sidebarElement.classList.remove('transform-translate', 'duration-150');
+        sidebarElement.classList.remove('-translate-x-full', 'translate-x-0', 'transform-translate', 'duration-150');
+        sidebarElement.style.transform = isOpen ? 'translateX(0)' : 'translateX(-100%)';
         
         overlayElement.classList.remove('invisible', 'transform-opacity', 'duration-150');
     });
     
     hammer.on('panmove', (e: HammerInput) => {
-        if (!dragging) return;
+        if (!dragging || surpassedBreakpoint) return;
 
         const width = getWidth();
         const delta = e.center.x - startX;
@@ -59,18 +77,42 @@ export function initializeSidebar(sidebarElement: HTMLElement, overlayElement: H
     });
     
     hammer.on('panend', (_e: HammerInput) => {
-        if (!dragging) return;
+        if (!dragging || surpassedBreakpoint) return;
+        
         dragging = false;
 
         sidebarElement.classList.add('transform-translate', 'duration-150');
+        sidebarElement.style.transform = '';
+        
         overlayElement.classList.add('transform-opacity', 'duration-150');
         translate > -50 ? open() : close();
     });
     
-    return hammer;
+    const onWindowResize = () => {
+        const wasSurpassedBreakpoint = surpassedBreakpoint;
+        surpassedBreakpoint = checkSurpassBreakpoint();
+        
+        if (wasSurpassedBreakpoint !== surpassedBreakpoint) {
+            if (surpassedBreakpoint) {
+                sidebarElement.classList.remove('-translate-x-full', 'translate-x-0');
+                close();
+            } else {
+                sidebarElement.classList.add(isOpen ? 'translate-x-0' : '-translate-x-full');
+                sidebarElement.style.transform = '';
+            }
+        }
+    };
+    
+    window.addEventListener('resize', onWindowResize);
+    
+    return {
+        hammer,
+        resizeUnregister: () => window.removeEventListener('resize', onWindowResize),
+    };
 }
 
-export function disposeSidebar(manager: HammerManager): void {
-    manager.destroy();
+export function disposeSidebar(disposeState: DisposeState): void {
+    disposeState.hammer.destroy();
+    disposeState.resizeUnregister();
 }
 
