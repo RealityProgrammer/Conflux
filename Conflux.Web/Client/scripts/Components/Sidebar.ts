7d93288585b1a63252a87fb87c80ec79de54ce3b
Caxivitual <destinyhero789@gmail.com>
2026-01-11 09:37:14 +0700

    refactor Sidebar.ts

--- Conflux.Web/Client/scripts/Components/Sidebar.ts
+++ Conflux.Web/Client/scripts/Components/Sidebar.ts
@@ -4,148 +4,168 @@ import {Interactable} from "@interactjs/core/Interactable";
 
 const MAX_OVERLAY_OPACITY = 60;
 
-interface DisposeState {
-    interactable: Interactable;
-    resizeUnregister: () => void;
-}
+const checkSurpassBreakpoint = () => {
+    return window.innerWidth >= 1024; // lg breakpoint
+};
 
-export function initializeSidebar(sidebarElement: HTMLElement, overlayElement: HTMLElement): DisposeState {
-    const checkSurpassBreakpoint = () => {
-        return window.innerWidth >= 1024; // lg breakpoint
-    };
+class Sidebar {
+    private readonly sidebarElement: HTMLElement;
+    private readonly overlayElement: HTMLElement;
+    private isOpen: boolean;
+    private dragging: boolean;
+    private translate: number;
+    private deltaX: number;
+    private surpassedBreakpoint: boolean;
+    private interactable: Interactable;
+    
+    constructor(sidebarElement: HTMLElement, overlayElement: HTMLElement) {
+        this.sidebarElement = sidebarElement;
+        this.overlayElement = overlayElement;
+        
+        this.isOpen = this.dragging = false;
+        this.translate = -100;
+        this.deltaX = 0;
+        
+        this.surpassedBreakpoint = checkSurpassBreakpoint();
+        
+        this.overlayElement.addEventListener("click", this.close);
+        window.addEventListener("resize", this.onWindowResize);
 
-    const handleResponsiveness = () => {
-        if (surpassedBreakpoint) {
-            sidebarElement.style.transform = '';
-            isOpen = false;
-            translate = 0;
-            overlayElement.classList.add('hidden');
+        this.interactable = interact(document.body)
+            .styleCursor(false)
+            .draggable({
+                inertia: false,
+                autoScroll: true,
+                modifiers: [
+                    // interact.modifiers.restrict({
+                    //     restriction: 'self',
+                    //     endOnly: false,
+                    //     elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+                    // })
+                ],
+                listeners: {
+                    start: this.onBeginDrag,
+                    move: this.onDragging,
+                    end: this.onEndDrag,
+                },
+            });
+    }
+    
+    handleResponsiveness = () => {
+        if (this.surpassedBreakpoint) {
+            this.sidebarElement.style.transform = '';
+            this.isOpen = false;
+            this.translate = 0;
+            this.overlayElement.classList.add('hidden');
         } else {
-            sidebarElement.style.transform = isOpen ? 'translateX(0)' : 'translateX(-100%)';
+            this.sidebarElement.style.transform = this.isOpen ? 'translateX(0)' : 'translateX(-100%)';
         }
     }
 
-    let isOpen = false, dragging = false, translate = -100, deltaX = 0;
-   
-    let surpassedBreakpoint = checkSurpassBreakpoint();
-   
-    const getWidth = () => sidebarElement.getBoundingClientRect().width;
+    getSidebarWidth = () => {
+        return this.sidebarElement.getBoundingClientRect().width;
+    }
 
-    const open = () => {
-        isOpen = true;
-        translate = 0;
+    open = () => {
+        this.isOpen = true;
+        this.translate = 0;
 
-        animate(sidebarElement, {
+        animate(this.sidebarElement, {
             x: '0%',
             duration: 150,
             ease: 'linear',
         });
-       
-        animate(overlayElement, {
+
+        animate(this.overlayElement, {
             opacity: `${MAX_OVERLAY_OPACITY / 100}`,
             duration: 150,
             ease: 'linear',
         });
-       
-        overlayElement.classList.remove('hidden');
+
+        this.overlayElement.classList.remove('hidden');
     };
 
-    const close = () => {
-        isOpen = false;
-        translate = -100;
+    close = () => {
+        this.isOpen = false;
+        this.translate = -100;
 
-        animate(sidebarElement, {
+        animate(this.sidebarElement, {
             x: '-100%',
             duration: 150,
             ease: 'linear',
             onComplete: () => {
-                sidebarElement.classList.add('-translate-x-full');
+                this.sidebarElement.classList.add('-translate-x-full');
             },
         });
-       
-        animate(overlayElement, {
+
+        animate(this.overlayElement, {
             opacity: `0%`,
             duration: 150,
             ease: 'linear',
         })
-       
-        overlayElement.classList.add('hidden');
+
+        this.overlayElement.classList.add('hidden');
     };
-
-    overlayElement.onclick = close;
-   
-    const interactable = interact(document.body);
     
-    document.body.classList.add('dragging-container')
+    onBeginDrag = (e: any) => {
+        if (this.surpassedBreakpoint || e.x0 > 100 || this.isOpen) return;
+
+        this.dragging = true;
+        this.deltaX = 0;
+
+        this.sidebarElement.style.transform = this.isOpen ? 'translateX(0)' : 'translateX(-100%)';
+        this.sidebarElement.classList.remove('-translate-x-full');
+        this.overlayElement.classList.remove('hidden');
+    }
     
-    interactable
-        .styleCursor(false)
-        .draggable({
-        inertia: false,
-        autoScroll: true,
-        modifiers: [
-            // interact.modifiers.restrict({
-            //     restriction: 'self',
-            //     endOnly: false,
-            //     elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-            // })
-        ],
-        listeners: {
-            start: (e: any) => {
-                if (surpassedBreakpoint || e.x0 > 100 || isOpen) return;
-                
-                dragging = true;
-                deltaX = 0;
-                
-                sidebarElement.style.transform = isOpen ? 'translateX(0)' : 'translateX(-100%)';
-                sidebarElement.classList.remove('-translate-x-full');
-                overlayElement.classList.remove('hidden');
-            },
-            move: (e: any) => {
-                if (!dragging || surpassedBreakpoint) return;
-                
-                const width = getWidth();
+    onDragging = (e: any) => {
+        if (!this.dragging || this.surpassedBreakpoint) return;
 
-                deltaX += e.dx;
-                
-                translate = isOpen ?
-                    Math.max(-100, Math.min(0, (deltaX / width) * 100)) :
-                    Math.max(-100, Math.min(0, -100 + (deltaX / width) * 100));
-                sidebarElement.style.transform = `translateX(${translate}%)`;
-                
-                // Remap translate from -100 to 0 into opacity 0 to MAX_OVERLAY_OPACITY.
-                const opacity = (translate + 100) * MAX_OVERLAY_OPACITY / 100;
-                overlayElement.style.opacity = `${opacity}%`;
-                overlayElement.classList.toggle('hidden', opacity <= 0);
-            },
-            end: (_e: any) => {
-                if (!dragging || surpassedBreakpoint) return;
+        const width = this.getSidebarWidth();
 
-                dragging = false;
-                
-                translate > -50 ? open() : close();
-            },
-        },
-    })
-   
-    const onWindowResize = () => {
-        const wasSurpassedBreakpoint = surpassedBreakpoint;
-        surpassedBreakpoint = checkSurpassBreakpoint();
-       
-        if (wasSurpassedBreakpoint !== surpassedBreakpoint) {
-            handleResponsiveness();
+        this.deltaX += e.dx;
+
+        this.translate = this.isOpen ?
+            Math.max(-100, Math.min(0, (this.deltaX / width) * 100)) :
+            Math.max(-100, Math.min(0, -100 + (this.deltaX / width) * 100));
+        this.sidebarElement.style.transform = `translateX(${this.translate}%)`;
+
+        // Remap translate from -100 to 0 into opacity 0 to MAX_OVERLAY_OPACITY.
+        const opacity = (this.translate + 100) * MAX_OVERLAY_OPACITY / 100;
+        this.overlayElement.style.opacity = `${opacity}%`;
+        this.overlayElement.classList.toggle('hidden', opacity <= 0);
+    }
+    
+    onEndDrag = (_e: any) => {
+        if (!this.dragging || this.surpassedBreakpoint) return;
+
+        this.dragging = false;
+
+        this.translate > -50 ? this.open() : this.close();
+    }
+
+    onWindowResize = () => {
+        const wasSurpassedBreakpoint = this.surpassedBreakpoint;
+        this.surpassedBreakpoint = checkSurpassBreakpoint();
+
+        if (wasSurpassedBreakpoint !== this.surpassedBreakpoint) {
+            this.handleResponsiveness();
         }
     };
-   
-    window.addEventListener('resize', onWindowResize);
-   
-    return {
-        interactable,
-        resizeUnregister: () => window.removeEventListener('resize', onWindowResize),
-    };
+    
+    dispose = () => {
+        this.interactable.unset();
+        this.overlayElement.removeEventListener("click", this.close);
+        window.removeEventListener("resize", this.onWindowResize);
+    }
 }
 
-export function disposeSidebar(disposeState: DisposeState): void {
-    disposeState.interactable.unset();
-    disposeState.resizeUnregister();
+export function initializeSidebar(sidebarElement: HTMLElement, overlayElement: HTMLElement): Sidebar {
+    document.body.classList.add('dragging-container')
+    
+    return new Sidebar(sidebarElement, overlayElement);
+}
+
+export function disposeSidebar(sidebar: Sidebar): void {
+    sidebar.dispose();
 }
