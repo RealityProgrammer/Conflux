066726deaa5d0c2fe17710101afea6c8deadd52f
Caxivitual <destinyhero789@gmail.com>
2026-01-17 10:26:52 +0700

    added IUserNotificationService

---
+++ Conflux.Application/Abstracts/IUserNotificationService.cs
@@ -0,0 +1,10 @@
+﻿using Conflux.Domain.Events;
+
+namespace Conflux.Application.Abstracts;
+
+public interface IUserNotificationService {
+    event Action<CommunityBannedEventArgs> OnCommunityBanned;
+
+    Task Connect();
+    Task Disconnect();
+}
